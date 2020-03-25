62776dd02fece36f5fabf0047621b3225e2dc069
Anand Gautam <Anand.Gautam@cognizant.com>
2020-03-25 18:46:39 +0530

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Searchbar from './components/Searchbar';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Iam APP COMPONENT
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <Searchbar />
       </div>
     );
   }
---
+++ src/components/Searchbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Searchbar extends React.Component {
+    state = {
+        term: ''
+    };
+    render() {
+        return (
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input
+                            value={this.state.term}
+                            type="text"
+                            onChange={e => this.setState({ term: e.target.value })}
+
+                        />
+                    </div>
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Searchbar;
