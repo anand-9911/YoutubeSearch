ac9dfb128fdb36e4a2970093bf193483b7d9d149
Anand Gautam <Anand.Gautam@cognizant.com>
2020-03-27 10:24:20 +0530

    Finishing the application

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -4,17 +4,24 @@ class Searchbar extends React.Component {
     state = {
         term: ''
     };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
     render() {
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form
+                    className="ui form"
+                    onSubmit={this.onFormSubmit}
+                >
                     <div className="field">
                         <label>Video Search</label>
                         <input
                             value={this.state.term}
                             type="text"
                             onChange={e => this.setState({ term: e.target.value })}
-
                         />
                     </div>
 
